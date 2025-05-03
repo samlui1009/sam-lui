c8642e6b9cc4d712520920820ed501762e96eda6
samlui1009 <sam_lui@hotmail.ca>
2025-05-03 14:44:22 -0700

    Properly aligned contact form with animated button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -18,20 +18,23 @@ function Contact() {
         </p>
         <img src={Image} className="contact-me-photo"></img>
         <p className="paragraph-2">
-            I'm looking forward to hearing from you! 
+            I'm looking forward to hearing from you soon! 
         </p>
         <form className="contact-form">
         <div className="input-box">
             <label>Full Name</label>
             <input type="text" className="field" placeholder="Enter your name" required />
         </div>
-        <div className="input-box">
+        <div className="input-box"> 
             <label>Email Address</label>
             <input type="text" className="field" placeholder="Enter your e-mail" required />
+            {/* These inputs are under the assumption for single-line inputs, like names and email fields
+            //  Thus, no resizing is necessary in this regard */}
         </div>
         <div className="input-box">
             <label>Write Your Message</label>
-            <input type="text" className="msg-field" placeholder="Write your message here" required />
+            <textarea className="msg-field" placeholder="Write your message here" required />
+            {/* Textarea is explicitly made for multi-line content like messages and comments, permitting resizing */}
         </div>
         <button type="submit" className="send-msg">Send Message</button>
         </form>
@@ -39,7 +42,6 @@ function Contact() {
         <Footer></Footer>
         </>
     )
-
 }
 
 export default Contact
